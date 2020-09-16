44918aec9745ca7f3e3eda7090c5452ded814498
baggio-snir <Quentin.Chassel@ac-lille.fr>
2020-09-16 18:51:19 +0200

    Internal name is a code, we do not have ht prices

--- scheme.sql
+++ scheme.sql
@@ -4,11 +4,10 @@ create database if not exists `ecommerce`;
 use `ecommerce`;
 
 create table `articles` (
+    `internalName` char(10) not null,
     `publicName` varchar(100) not null,
-    `internalName` varchar(100) not null,
-    `unitPriceHT` decimal(5,2) not null,
+    `unitPriceHT` decimal(5,2) default null,
     `unitPriceTTC` decimal(5,2) not null,
     `description` text not null,
     `stock` int default null
 );
-
