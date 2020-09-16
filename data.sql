56568c5bbb014a18e2ff63e90110b9d83129efc3
baggio-snir <Quentin.Chassel@ac-lille.fr>
2020-09-16 18:52:27 +0200

    Add articles initial data

---
+++ data.sql
@@ -0,0 +1,17 @@
+
+insert into `articles`
+(`internalName`, `publicName`, `unitPriceTTC`, `description`)
+values
+('ADSRF', 'Audiosurf', 9.99, 'Jeu sous-estimé'),
+('BRDL', 'Borderlands', 9.87, 'Aussi nommé Bordelands'),
+('CTSK', 'Cities: Skyline', 27.99, 'Refaire Lille en mieux'),
+('DTSTRV', 'Don''t Starve', 8.19, 'Burton avait raison'),
+('FCTR', 'Factorio', 25, 'Comment perdre sa nuit'),
+('FG$$', 'Fall Guys: Ultimate Knockout', 19.99, "On a besoin d'argent
+
+Donc on a fait ça"),
+('GRIS', 'GRIS', 16.99, 'Un peu d''indépendant, ça fait du bien'),
+('HTK', 'Helltaker', 0, 'Les vrais sauront'),
+('L4D', 'Left 4 Dead', 8.19, 'Les 4 couleurs primaires'),
+('MGS5S', 'METAL GEAR SOLID V : THE PHANTOM MENACE REMASTERED SPECIAL EDITION', 45, 'Snake ? Snaaaaaake !'),
+('ROR2', 'Risk of Rain 2', 24.99, 'JE VOUS VOIS');
